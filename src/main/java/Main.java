ed457f2869656cadfedcda4d8b303b090f1cb662
Марсель <marsel21vek@yandex.ru>
2023-11-27 10:54:49 +0500

    добавил клас Calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На какое количество человек Вы планируете разделить счёт?");
+        int amounthPersons = scanner.nextInt();
+        if (amounthPersons => 1){
+            Calculator.calculator();
+        } else {
+                System.out.println("Некорректное значение! Пожалуйста, повторите ввод.");
+                amounthPersons = scanner.nextInt();
+            }
     }
 }
