0f2b0adebe673c981e1ce8046259ee45582ad006
Марсель <marsel21vek@yandex.ru>
2023-11-28 15:35:46 +0500

    Работает количество человек

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите количество человек:");
+             amountPersons();
+    }
+
+
+    public static void amountPersons() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("На какое количество человек Вы планируете разделить счёт?");
-        if (scanner.hasNextInt()) {
+        while (scanner.hasNextInt()) {
             int amountPersons = scanner.nextInt();
-            if (amountPersons > 1) {
-                System.out.println("больше 1");
-            } else if (amountPersons <= 1) {
-                System.out.println("Некорректное значение! Пожалуйста, повторите ввод.");
-            }
-
-
-        }
+            if (amountPersons >= 2) {
+                System.out.println("ввели больше 2");
+            }else {System.out.println("введите больше 1");}
+        } System.out.println("введите целое число");
+        amountPersons();
     }
 }
-
-//amounthPersons <= 1
-//  Calculator.calculator();
-//  System.out.println("Некорректное значение! Пожалуйста, повторите ввод.");
