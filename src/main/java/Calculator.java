cc484557c6b52b48fd33aad921ed5ef389ee2b5e
Marsel <marsel21vek@yandex.ru>
2023-11-30 00:12:29 +0500

    ввод пользователя

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,49 +1,53 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static int amountPersons() {
+    public static void calculator() {
         Scanner scanner = new Scanner(System.in);
+        int persons = 0;
+        String menuSum = " ";
+        double priceSum = 0;
+
         System.out.println("Введите количество человек:");
         while (true) {
             if (scanner.hasNextInt()) {
-                int persons = scanner.nextInt();
-                if (persons > 1) {
-                    return persons;
-                } //else if (persons <= 1) {
+                persons = scanner.nextInt();
+                if (persons < 2) {
                     System.out.println("Введите число больше 1");
-                //}
+                } else {
+                    break;
+                }
             } else {
                 System.out.println("Введите число");
                 scanner.next();
             }
         }
-    }
+        // System.out.println("Ввели " + persons);
 
-    public static String menu() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название товара:");
-        while (true) {
-            if (scanner.hasNextLine()) {
-                String menu = scanner.nextLine();
-                return menu;
-            } else {
-                System.out.println("Введите текст");
-                scanner.next();
-            }
-        }
-    }
 
-    public static Double price() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите цену товара:");
         while (true) {
-            if (scanner.hasNextDouble()) {
-                Double price = scanner.nextDouble();
-                return price;
-            } else {
-                System.out.println("Введите число");
-                scanner.next();
-            }
-        }
+            //  if (scanner.hasNextLine()) {
+
+            String menu = scanner.nextLine();
+            System.out.println("Введите название товара или введите 'Завершить', что бы выйти");
+            // if (menu.equalsIgnoreCase("Завершить")) {
+            //       break;
+
+            //
+            //else {
+
+            double price = scanner.nextDouble();
+            System.out.println("Введите цену товара:");
+            //System.out.println("Введите текст");
+            // if (scanner.hasNextDouble()) {
+
+
+            // priceSum += price;
+            //menuSum += menu + "\n";
+
+        } //else {
+        //System.out.println("Введите число");
     }
 }
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Calculator.amountPersons() + " человек");
-        System.out.println("Товар " + Calculator.menu());
-        System.out.println("Цена" + Calculator.price());
 
+
+        Calculator.calculator();
 
 
     }
