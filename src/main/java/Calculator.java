9c51a9cd69aa4cd96c28ab64ccf7a9a60f314d73
Marsel <marsel21vek@yandex.ru>
2023-12-01 00:10:42 +0500

    ввод пользователя РАБОТАЕТ

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
         int persons = 0;
         String menuSum = " ";
         double priceSum = 0;
-
         System.out.println("Введите количество человек:");
         while (true) {
             if (scanner.hasNextInt()) {
@@ -21,32 +20,34 @@ public class Calculator {
                 scanner.next();
             }
         }
-        // System.out.println("Ввели " + persons);
-
-
         while (true) {
             //  if (scanner.hasNextLine()) {
-
-            String menu = scanner.nextLine();
             System.out.println("Введите название товара или введите 'Завершить', что бы выйти");
-            // if (menu.equalsIgnoreCase("Завершить")) {
-            //       break;
-
-            //
-            //else {
-
+            scanner.nextLine();
+            String menu = scanner.nextLine();
+            if (menu.equalsIgnoreCase("Завершить")) {
+                System.out.println("Товар успешно добавлен.");
+                break;
+            } menuSum += menu + "\n";
+            while (true) {
+             System.out.println("Введите цену товара:");
+                if (scanner.hasNextDouble()) {
             double price = scanner.nextDouble();
-            System.out.println("Введите цену товара:");
-            //System.out.println("Введите текст");
-            // if (scanner.hasNextDouble()) {
-
-
-            // priceSum += price;
-            //menuSum += menu + "\n";
-
-        } //else {
-        //System.out.println("Введите число");
+                    if (price > 0) {
+                         priceSum += price;
+                            break;
+                        }else {
+    System.out.println("Введите число больше 0");
+                }
+                }else {
+                    System.out.println("Введите число");
+                    scanner.next();
+                }
+        }
     }
+        System.out.println(menuSum);
+        System.out.print("Каждый человек должен заплатить:" + priceSum / persons);
+}
 }
 
 
