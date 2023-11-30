96771fd7df910404e0bed7f8678a3be628d4ec74
Marsel <marsel21vek@yandex.ru>
2023-12-01 00:10:42 +0500

    ввод пользователя РАБОТАЕТ

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
         int persons = 0;
         String menuSum = " ";
         double priceSum = 0;
-
         System.out.println("Введите количество человек:");
         while (true) {
             if (scanner.hasNextInt()) {
@@ -21,32 +20,27 @@ public class Calculator {
                 scanner.next();
             }
         }
-        // System.out.println("Ввели " + persons);
-
-
         while (true) {
             //  if (scanner.hasNextLine()) {
-
-            String menu = scanner.nextLine();
             System.out.println("Введите название товара или введите 'Завершить', что бы выйти");
-            // if (menu.equalsIgnoreCase("Завершить")) {
-            //       break;
-
-            //
-            //else {
-
+            scanner.nextLine();
+            String menu = scanner.nextLine();
+            if (menu.equalsIgnoreCase("Завершить")) {
+                break;
+            } menuSum += menu + "\n";
+            while (true) {
+             System.out.println("Введите цену товара:");
+                if (scanner.hasNextDouble()) {
             double price = scanner.nextDouble();
-            System.out.println("Введите цену товара:");
-            //System.out.println("Введите текст");
-            // if (scanner.hasNextDouble()) {
-
-
-            // priceSum += price;
-            //menuSum += menu + "\n";
-
-        } //else {
-        //System.out.println("Введите число");
-    }
+            priceSum += price;
+            break;
+                }else {
+                    System.out.println("Введите число");
+                    scanner.next();
+                }
+        }
+    } System.out.println(priceSum + menuSum);
+}
 }
 
 
